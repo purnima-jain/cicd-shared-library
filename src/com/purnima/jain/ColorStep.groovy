cd7dafc0f23d938ee51e9cfbe32656aed9888f56
Purnima Jain <jain_purnima@hotmail.com>
2024-12-24 22:35:58 +0530

    Fixed Code

--- src/com/purnima/jain/ColorStep.groovy
+++ src/com/purnima/jain/ColorStep.groovy
@@ -3,7 +3,7 @@ package com.purnima.jain
 def green(String script) {
     echo "${script}"
     ansiColor('xterm') {
-        echo "\033[31;1m ${script} \033[0m"
+        echo "\033[1;32m${script}\033[0m"
     }
     
 }
