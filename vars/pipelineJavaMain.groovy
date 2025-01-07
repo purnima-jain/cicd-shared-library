9648f87bc5d13f1031889d702a346d05e7a36307
Purnima Jain <jain_purnima@hotmail.com>
2025-01-07 20:11:56 +0530

    Added Code

--- vars/pipelineJavaMain.groovy
+++ vars/pipelineJavaMain.groovy
@@ -25,14 +25,37 @@ def call(Map pipelineCfg = [:]) {
                             devopsMetadataBranch: pipelineCfg.devopsMetadataBranch // pipelineCfg.devopsMetadataBranch: master
                         )
         }
-
-        // AnsiColor at pipeline level
+        
         options {
-            ansiColor('xterm')
+            ansiColor('xterm') // AnsiColor at pipeline level
+            timestamps()
+            disableConcurrentBuilds()
+            timeout(time: 30, unit: 'MINUTES')
+            buildDiscarder(logRotator(
+                numToKeepStr: '15',
+                daysToKeepStr: '-1',
+                artifactNumToKeepStr: '-1',
+                artifactDaysToKeepStr: '-1'
+            ))
         }
 
+        environment {
+            BASEWORKPATH = "${pwd()}/work"
+
+            def gitUrl = "https://github.com/purnima-jain/business-application-payments-daily.git" // env.GIT_URL but hard-coding it temporarily
+            echo "gitUrl: ${gitUrl}" // gitUrl: https://github.com/purnima-jain/business-application-payments-daily.git
+
+            def name = gitUrl.replaceFirst(/^.*\/([^\/]+).git$/, '$1')
+            echo "name: ${name}" // name: business-application-payments-daily
+
+            def domain = evaluateDomain(name)
+            echo "domain: ${domain}" // domain: payments
+        }
+
+
+
         stages {
-            stage("clean workspace") {
+            stage("Clean Workspace") {
                 steps {
                     script {
                         sh "ls"
@@ -44,22 +67,46 @@ def call(Map pipelineCfg = [:]) {
             stage("Initialize Pipeline") {
                 steps {
                     script {
-                        ColorStep.green("Initialize Pipeline has started......")
+
                         echo "configChange: " + configChange // configChange: false
                         echo "executeSast: " + executeSast   // executeSast: false
 
-                        echo "gitUrl: " + env.GIT_URL              // gitUrl: https://github.com/purnima-jain/cicd-pipelines.git
-                        echo "githubCredentialsId: " + githubCredentialsId // githubCredentialsId: GITHUB_CREDENTIAL_ID
-                        echo "env.BRANCH_NAME: " + env.BRANCH_NAME // env.BRANCH_NAME: null Expl: This variable only works in a multibranch pipline
-                        echo "pipelineCfg.devopsMetadataBranch: " + pipelineCfg.devopsMetadataBranch // pipelineCfg.devopsMetadataBranch: master
+                        withCredentials([
+                            usernamePassword(credentialsId: "${nexusCredId}", usernameVariable: 'NEXUS_SERVICEACC_USERNAME', passwordVariable: 'NEXUS_SERVICEACC_TOKEN'),
+                            usernamePassword(credentialsId: "${githubCredentialsId}", usernameVariable: 'GIT_SERVICEACC_USERNAME', passwordVariable: 'GIT_SERVICEACC_TOKEN')
+                        ]) {
+                            echo "GIT_SERVICEACC_USERNAME: ${GIT_SERVICEACC_USERNAME}"
+                            echo "GIT_SERVICEACC_TOKEN: ${GIT_SERVICEACC_TOKEN}"
+                            
+                            echo "NEXUS_SERVICEACC_USERNAME: ${NEXUS_SERVICEACC_USERNAME}"
+                            echo "NEXUS_SERVICEACC_TOKEN: ${NEXUS_SERVICEACC_TOKEN}"
 
-                        /*def jdkVersion = getPipelineTools(
-                            gitUrl: "https://github.com/purnima-jain/business-application-payments-daily.git", // env.GIT_URL, Hardcoding it temporarily
-                            githubCredentialsId: githubCredentialsId,
-                            branch: "master",               // Hardcoding it temporarily env.BRANCH_NAME,
-                            devopsMetadataBranch: pipelineCfg.devopsMetadataBranch
-                        )
-                        echo "jdkVersion: " + jdkVersion */
+                            echo "gitUrl: " + env.GIT_URL              // gitUrl: https://github.com/purnima-jain/cicd-pipelines.git
+                            echo "githubCredentialsId: " + githubCredentialsId // githubCredentialsId: GITHUB_CREDENTIAL_ID
+                            echo "env.BRANCH_NAME: " + env.BRANCH_NAME // env.BRANCH_NAME: null Expl: This variable only works in a multibranch pipline
+                            echo "pipelineCfg.devopsMetadataBranch: " + pipelineCfg.devopsMetadataBranch // pipelineCfg.devopsMetadataBranch: master
+                            def jdkVersion = getPipelineTools(
+                                gitUrl: "https://github.com/purnima-jain/business-application-payments-daily.git", // env.GIT_URL, Hardcoding it temporarily
+                                githubCredentialsId: githubCredentialsId,
+                                branch: "master",               // Hardcoding it temporarily env.BRANCH_NAME,
+                                devopsMetadataBranch: pipelineCfg.devopsMetadataBranch
+                            )
+                            echo "jdkVersion: " + jdkVersion // jdkVersion: 17
+
+                            ColorStep.green("Initialize Stage has started......")
+                        }
+
+
+
+
+
+
+                        
+                        
+
+                        
+
+                        
 
                         
                     }                    
